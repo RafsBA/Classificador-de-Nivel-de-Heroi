3e7f9a7826ba6b0f56a706ab6c1173eb7b397b1d
RafsBA <rafaelsant.oliv@gmail.com>
2023-12-11 13:01:29 -0300

    socorro

---
+++ source/hero.js
@@ -0,0 +1,11 @@
+let nivel = 0
+let heroi = "Rafael S."
+let treinar = ("O heroi " + heroi + " precisa treinar mais para atingir o INFINITO E ALÉM!" )
+
+for (i = 0; i < 1000; i + 1001){
+    
+if (nivel < 1000) 
+    console.log("Seu nível é muito abaixo do esperado, você está muito fraco jovem, seu nível atual é: Bronze")
+    console.log(treinar) 
+}
+
